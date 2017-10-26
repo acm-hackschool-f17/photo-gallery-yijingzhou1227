0248217fb766e78ebc9eaa6fd8a8fd069110fc4d
Yijing Zhou <ashes@s-169-232-241-164.resnet.ucla.edu>
2017-10-25 19:52:39 -0700

    update website

--- script.js
+++ script.js
@@ -8,22 +8,31 @@ function handleImageClick(event) {
 	const images = $('.photo-row .image');
 	const info = $('.info-pane');
 	const preview = $('.preview-image');
+	const pop_up = $(".pop_up");
 
 	if (target.hasClass('selected')) {
+
 		images.removeClass('not-selected');
 		target.removeClass('selected');
-		info.fadeOut(200);
+		pop_up.hide();
+		info.fadeOut(200); 
+
+
 	}
 	else {
 		images.removeClass('selected');
 		images.addClass('not-selected');
-
 		target.addClass('selected');
 		target.removeClass('not-selected');
-
-		info.fadeIn(200);
 		preview.css('background-image', target.css('background-image'));
+		info.fadeIn(200);
+		pop_up.hide();
+
+
 	}
+    
+    
+
 }
 
 $('document').ready(function() {
